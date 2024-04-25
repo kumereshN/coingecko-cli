f3fbe7e13a05219837b88ef3577247d1b23843b1
kumereshN <troggozor@outlook.com>
2024-04-25 21:36:34 +0800

    done formatting output of cli

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,12 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(name = "coingecko-cli",version = "1.0", about = "CoinGecko API CLI tool", long_about = None)]
-struct Cli {
+pub struct Cli {
     #[arg(short, long = "coin–name")]
     name: String,
-    #[arg(short, long = "currency")]
+    #[arg(short, long = "currency", default_value = "sgd")]
     currency: String,
-    #[arg(short, long)]
-    #[arg(default_value = "2")]
+    #[arg(short, long, default_value = "2")]
     precision: String
 }
 
@@ -23,12 +22,21 @@ struct MyConfig {
     api_key: String,
 }
 
+impl Default for MyConfig {
+    fn default() -> Self {
+        Self {
+            version: 0,
+            coingecko_api_url: String::from("https://api.coingecko.com/api/v3"),
+            api_key: String::from("")
+        }
+    }
+}
+
 #[derive(Deserialize, Debug)]
-struct Resp {
+pub struct Resp {
     n_currencies: Option<usize>,
     #[serde(flatten)]
     prices: HashMap<String, HashMap<String, f64>>
-
 }
 
 impl fmt::Display for Resp {
@@ -48,22 +56,13 @@ impl fmt::Display for Resp {
     }
 }
 
-
-impl Default for MyConfig {
-    fn default() -> Self {
-        Self {
-            version: 0,
-            coingecko_api_url: String::from("https://api.coingecko.com/api/v3"),
-            api_key: String::from("")
-        }
-    }
+async fn get_all_currencies(cfg: &MyConfig) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let coin_args = format!("{}/simple/supported_vs_currencies?x_cg_demo_api_key={}",
+                            &cfg.coingecko_api_url, &cfg.api_key);
+    let response = reqwest::get(coin_args).await?;
+    let obj = response.json::<Vec<String>>().await?;
+    Ok(obj)
 }
-
-#[derive(Debug, Serialize)]
-pub enum CoinGeckoEndpoints {
-    Simple(u32)
-}
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -71,11 +70,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg: MyConfig = confy::load("coingecko-config", "test1")?;
     let cli = Cli::parse();
 
+    let vec_currencies = get_all_currencies(&cfg).await?;
+    let cli_currency = cli.currency
+        .split(',')
+        .filter(|&c| vec_currencies.contains(&c.to_string()))
+        .collect::<Vec<&str>>()
+        .join(",");
     let coin_args = format!("{}/simple/price?x_cg_demo_api_key={}&ids={}&vs_currencies={}&precision={}",
-                            &cfg.coingecko_api_url, &cfg.api_key, &cli.name, &cli.currency, &cli.precision);
+                            &cfg.coingecko_api_url, &cfg.api_key, &cli.name, &cli_currency, &cli.precision);
     let response = reqwest::get(coin_args).await?;
     let mut obj: Resp = response.json().await?;
-    obj.n_currencies = Some(cli.currency.chars().filter(|&c| c == ',').count());
+    obj.n_currencies = Some(cli_currency.chars().filter(|&c| c == ',').count());
 
     println!("{}", obj);
     Ok(())
