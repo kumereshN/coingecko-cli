97df291fbea4cf7358fad0318aff28c218c436d5
kumereshN <troggozor@outlook.com>
2024-04-26 18:19:42 +0800

    added fees and anyhow crate

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn calculate_fees(fees: f32, current_holdings: f32) -> f32 {
+    current_holdings * fees
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use thousands::Separable;
+use coingecko_cli::calculate_fees;
 use std::fmt;
+use std::fmt::{Debug};
 use serde::{Deserialize, Serialize};
-use clap::Parser;
+use anyhow::{anyhow, Context, Result};
+use clap::{Parser};
 
 #[derive(Parser)]
 #[command(name = "coingecko-cli",version = "1.0", about = "CoinGecko API CLI tool", long_about = None)]
@@ -11,8 +14,12 @@ pub struct Cli {
     name: String,
     #[arg(short, long = "currency", default_value = "sgd")]
     currency: String,
+    #[arg(short = 'a', long = "current-amount")]
+    current_amount: f32,
     #[arg(short, long, default_value = "2")]
-    precision: String
+    precision: String,
+    #[arg(short, long, default_value = "0.0006")]
+    fees: Option<f32>
 }
 
 #[derive(Serialize, Deserialize)]
@@ -36,19 +43,21 @@ impl Default for MyConfig {
 pub struct Resp {
     n_currencies: Option<usize>,
     #[serde(flatten)]
-    prices: HashMap<String, HashMap<String, f64>>
+    prices: HashMap<String, HashMap<String, f64>>,
+    fees: Option<f32>
 }
 
 impl fmt::Display for Resp {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let n = &self.n_currencies.unwrap();
+        let fees = &self.fees.unwrap();
         for (coin, prices) in &self.prices {
-            write!(f, "The price of {}", coin)?;
+            write!(f, "The current price of {}", coin)?;
             for (i, (currency, price)) in prices.iter().enumerate() {
                 if i == *n {
-                    writeln!(f, " in {} is ${}.", currency, price.separate_with_commas())?
+                    writeln!(f, " in {} is ${} and the fees are ${:.2}.", currency, price.separate_with_commas(), fees)?
                 } else {
-                    write!(f, " in {} is ${},", currency, price.separate_with_commas())?
+                    write!(f, " in {} is ${} and the fees are ${:.2}", currency, price.separate_with_commas(), fees)?
                 }
             }
         }
@@ -56,7 +65,7 @@ impl fmt::Display for Resp {
     }
 }
 
-async fn get_all_currencies(cfg: &MyConfig) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+async fn get_all_currencies(cfg: &MyConfig) -> Result<Vec<String>> {
     let coin_args = format!("{}/simple/supported_vs_currencies?x_cg_demo_api_key={}",
                             &cfg.coingecko_api_url, &cfg.api_key);
     let response = reqwest::get(coin_args).await?;
@@ -65,22 +74,41 @@ async fn get_all_currencies(cfg: &MyConfig) -> Result<Vec<String>, Box<dyn std::
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     // The config file can be found in C:\Users\Kumeresh\AppData\Roaming\coingecko-config
     let cfg: MyConfig = confy::load("coingecko-config", "test1")?;
     let cli = Cli::parse();
 
-    let vec_currencies = get_all_currencies(&cfg).await?;
+
+    if cli.name.contains(','){
+        return Err(anyhow!("Invalid character found: ,"))
+    }
+
+    let vec_currencies = get_all_currencies(&cfg).await.context("Unable to get currencies")?;
     let cli_currency = cli.currency
         .split(',')
-        .filter(|&c| vec_currencies.contains(&c.to_string()))
+        .collect::<HashSet<&str>>()
+        .iter()
+        .filter_map(|&c| {
+            match vec_currencies.contains(&c.to_string()) {
+                true => Some(c),
+                false => None
+            }
+        })
         .collect::<Vec<&str>>()
         .join(",");
     let coin_args = format!("{}/simple/price?x_cg_demo_api_key={}&ids={}&vs_currencies={}&precision={}",
                             &cfg.coingecko_api_url, &cfg.api_key, &cli.name, &cli_currency, &cli.precision);
-    let response = reqwest::get(coin_args).await?;
-    let mut obj: Resp = response.json().await?;
+    let response = reqwest::get(coin_args).await.context("Unable to obtain a response")?;
+    let mut obj: Resp = response.json().await.context("Unable to parse response object to json")?;
+    let price_of_coin = *obj.prices
+        .get(&cli.name)
+        .with_context(|| format!("Name of the coin: {} is incorrect", &cli.name))?
+        .get(&cli_currency)
+        .context("Unable to obtain the currency of the coin")? as f32;
+    let fees = calculate_fees(cli.fees.unwrap(), price_of_coin);
     obj.n_currencies = Some(cli_currency.chars().filter(|&c| c == ',').count());
+    obj.fees = Some(fees);
 
     println!("{}", obj);
     Ok(())
